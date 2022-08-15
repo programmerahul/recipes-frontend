aab0fa2533470e8be34b7db40f17de0ac7e2c8ca
Rahul-Prasad <programmerahul@gmail.com>
2022-08-15 16:05:04 +0530

    customers and rentals deleted

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -16,16 +16,6 @@ class NavBar extends Component {
                 Movies
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers">
-                Customers
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rentals">
-                Rentals
-              </NavLink>
-            </li>
             {!user && (
               <React.Fragment>
                 <li className="nav-item">
