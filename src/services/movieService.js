03b291da446d81a33ddd1161f16ddd18fc0ceb4e
Rahul-Prasad <programmerahul@gmail.com>
2022-08-23 14:45:41 +0530

    like bug solved

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,7 +2,6 @@ import httpServices from "./httpServices";
 const url = "/movies";
 export async function getMovies() {
   const { data: movies } = await httpServices.get(url);
-
   return movies;
 }
 
@@ -14,9 +13,13 @@ export async function saveMovie(movie) {
   if (movie.genreId === "") return null;
   let movieInDb = {};
   if (movie._id) {
-    const id = movie._id;
-    delete movie._id;
-    movieInDb = await httpServices.put(url + "/" + id, movie).data;
+    const movieCopy = { ...movie };
+    const id = movieCopy._id;
+    delete movieCopy._id;
+    const genreId = movieCopy.genre._id;
+    delete movieCopy.genre;
+    movieCopy.genreId = genreId;
+    movieInDb = await httpServices.put(url + "/" + id, movieCopy).data;
   } else {
     movieInDb = await httpServices.post(url, movie).data;
   }
